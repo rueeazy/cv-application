9d28469b28c0df5987c1e983f663ce399744ea86
Christian Ruiz <santacruz.crm@gmail.com>
2020-09-29 12:49:36 -0700

    Updates

---
+++ precache-manifest.b739db1f553f18a5b4c2438a8de7fa9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a772c4007d62a0720ae7039e9618ae",
+    "url": "/cv-application/index.html"
+  },
+  {
+    "revision": "a01be15223b66b6b1dd8",
+    "url": "/cv-application/static/css/main.1bc451f6.chunk.css"
+  },
+  {
+    "revision": "2fabc810add9790c794a",
+    "url": "/cv-application/static/js/2.57a08eb9.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/cv-application/static/js/2.57a08eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a01be15223b66b6b1dd8",
+    "url": "/cv-application/static/js/main.7660fafd.chunk.js"
+  },
+  {
+    "revision": "4a69fa3c6cf210a1d2d8",
+    "url": "/cv-application/static/js/runtime-main.26e5b48e.js"
+  }
+]);
